8b792bed3227ec0563bf620c2fe49761f3911460
BaicanIulia <iulia.baican@qubiz.com>
2024-11-14 13:10:38 +0200

    changed generic functional comp to class comp

--- src/components/Dropdown/component.tsx
+++ src/components/Dropdown/component.tsx
@@ -5,6 +5,7 @@ import {
   SelectChangeEvent,
   MenuItem,
 } from '@mui/material';
+import { Component } from 'react';
 
 type Option = {
   value: string;
@@ -19,29 +20,27 @@ type DropdownProps = {
   options: Option[];
 };
 
-export const Dropdown = ({
-  handleChange,
-  value,
-  name,
-  label,
-  options,
-}: DropdownProps) => {
-  return (
-    <FormControl fullWidth>
-      <InputLabel id={`${label}-select-label`}>{label}</InputLabel>
-      <Select
-        id={`${name}-select`}
-        value={value}
-        label={label}
-        onChange={handleChange}
-        name={name}
-      >
-        {options.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  );
-};
+export class Dropdown extends Component<DropdownProps> {
+  render() {
+    const { value, name, label, options, handleChange } = this.props;
+
+    return (
+      <FormControl fullWidth>
+        <InputLabel id={`${label}-select-label`}>{label}</InputLabel>
+        <Select
+          id={`${name}-select`}
+          value={value}
+          label={label}
+          onChange={handleChange}
+          name={name}
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    );
+  }
+}
